9d9ca3a55de61602a45ea032d0eb2d8bf0654e78
G Venkata Narasimha <gvenkatnarasimh965@gmail.com>
2025-11-28 23:50:52 +0530

    Update api.ts

--- Frontend/src/api/api.ts
+++ Frontend/src/api/api.ts
@@ -1,8 +1,7 @@
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://cancerprognosis.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
